4d4faada345261f919636965b897e824a78ce581
nsankabathula <naveen.c.s@hotmail.com>
2018-09-03 14:54:22 -0400

    all commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import { AppService } from 'src/app/services/app.service';
+import { FileItem, TLineItem, PLineItem, Accuracy } from 'src/app/models/model';
+import { Observable } from 'rxjs';
+import { forkJoin } from "rxjs";
+import { error } from 'util';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private appService: AppService) {
+
+
+  }
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';  // replaces previous Http service
+import { AppService } from 'src/app/services/app.service';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from 'src/app/common/nav.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { TrainingComponent } from 'src/app/training/training.component';
+import { TrainingService } from 'src/app/services/training.service';
+import { HomeComponent } from 'src/app/common/home.component';
+import { AppBootstrapModule } from 'src/app/app-bootstrap.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CompareAllComponent } from 'src/app/compare/compare-all.component';
+import { CompareOneComponent } from 'src/app/compare/compare-one.component';
+import { CompareService } from 'src/app/services/compare.service';
+import { CoreEsService } from 'src/app/services/es.service';
+import { CreditEsService } from 'src/app/services/es-credit.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TrainingComponent,
+    HomeComponent,
+    CompareAllComponent,
+    CompareOneComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    //NgbModule.forRoot(),
+    AppRoutingModule,
+    AppBootstrapModule,
+    FormsModule,
+    ReactiveFormsModule,
+
   ],
-  providers: [],
+  providers: [AppService, TrainingService, CompareService, CoreEsService, CreditEsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private appSvc: AppService) {
+    //console.log(appSvc.getFileNames())
+  }
+}
